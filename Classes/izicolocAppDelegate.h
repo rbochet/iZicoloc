2b5b5a0e29f6115b9271ff02ee1c7dad538a691f
Romain Bochet <rom@bochet.fr>
2011-01-19 22:22:44 +0100

    Initial commit

---
+++ Classes/izicolocAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  izicolocAppDelegate.h
+//  izicoloc
+//
+//  Created by rt on 19/01/11.
+//  Copyright 2011 Insa de Lyon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface izicolocAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  izicoloc
+//
+//  Created by rt on 19/01/11.
+//  Copyright 2011 Insa de Lyon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
